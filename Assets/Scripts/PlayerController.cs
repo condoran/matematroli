e6f351b08522da99e676ff3515d546a0aa7d52a7
Condoran <anco990@gmail.com>
2021-03-21 17:35:38 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 2.5f;
+    [SerializeField] private float moveSpeed = 1.5f;
+
+    public GameObject text;
+    private float rotateX;
+    private float dirZ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotateX = Input.GetAxis("Horizontal") * rotationSpeed;
+        dirZ = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Translate(Vector3.forward * dirZ * Time.deltaTime);
+        transform.Rotate(0, rotateX, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tree"))
+        {
+            text.SetActive(true);
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Tree"))
+        {
+            text.SetActive(false);
+        }
+    }
+}
