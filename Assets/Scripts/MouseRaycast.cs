6dc288e568518411e0d96af727468db30b5de680
Condoran <anco990@gmail.com>
2021-03-26 17:31:28 +0200

    spaceship moves toward planet

---
+++ Assets/Scripts/MouseRaycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRaycast : MonoBehaviour
+{
+    private MoveSpaceshipToPlanet script;
+    private Camera camera;
+    void Start()
+    {
+        camera = this.transform.GetComponent<Camera>();
+        script = GameObject.Find("Spaceship").GetComponent<MoveSpaceshipToPlanet>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            
+            
+            if (objectHit.CompareTag("Planet"))
+            {
+                if (Input.GetButton("Fire1"))
+                {
+                    script.MoveSpaceshipTo(objectHit);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveSpaceshipToPlanet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSpaceshipToPlanet : MonoBehaviour
+{
+    public Transform spaceship;
+    
+    [SerializeField] private float speed;
+
+    private Transform planet;
+    private bool isMooving;
+
+    private Vector3 targetPoint;
+
+    private Quaternion targetRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMooving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMooving)
+        {
+            
+            float step =  speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, planet.position, step);
+
+            // Check if the position of the cube and sphere are approximately equal.
+            if (Vector3.Distance(transform.position, planet.position) < 18f)
+            {
+                isMooving = false;
+                Debug.Log(Vector3.Distance(transform.position, planet.position));
+            }
+        }
+    }
+
+    public void MoveSpaceshipTo(Transform planet)
+    {
+        this.planet = planet;
+        isMooving = true;
+    }
+}
