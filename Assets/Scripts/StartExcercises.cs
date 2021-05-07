1cc5b5349eedb5be97b9ff24d9cf8ba15584e19b
Condor Andrei <44895249+condoran@users.noreply.github.com>
2021-05-07 09:58:20 +0300

    game logic

--- Assets/Scripts/StartExcercises.cs
+++ Assets/Scripts/StartExcercises.cs
@@ -11,6 +11,9 @@ public class StartExcercises : MonoBehaviour
     private HttpPostService service;
     public AudioSource audio;
     public RawImage img;
+    public Button buttonA, buttonB, buttonC, buttonD;
+    public Text quizText;
+    public GameObject quizImg;
     private AudioClip lamusic;
 
     void Start()
@@ -20,10 +23,54 @@ public class StartExcercises : MonoBehaviour
 
     public void Excercise()
     {
+        int sol;
         var result = Task.Run(() => service.GetExercise()).Result;
         StartCoroutine(GetAudioClip(result[0].Properties.HelperOne.Audio));
-        Debug.Log(result[0].Properties.HelperOne.Images[0].Url);
-        StartCoroutine(DownloadImage(result[0].Properties.HelperOne.Images[0].Url));
+        Debug.Log(result[0].Properties.OptionOne);
+        Debug.Log(result[0].Properties.OptionTwo);
+        Debug.Log(result[0].Properties.OptionThree);
+        Debug.Log(result[0].Properties.OptionFour);
+        Debug.Log(result[0].Properties.Solution);
+        quizImg.SetActive(true);
+        quizText.text = result[0].Properties.Editor;
+        foreach (Transform child in buttonA.transform)
+        {
+            child.gameObject.GetComponent<Text>().text = result[0].Properties.OptionOne;
+        }
+        
+        foreach (Transform child in buttonB.transform)
+        {
+            child.gameObject.GetComponent<Text>().text = result[0].Properties.OptionTwo;
+        }
+        
+        foreach (Transform child in buttonC.transform)
+        {
+            child.gameObject.GetComponent<Text>().text = result[0].Properties.OptionThree;
+        }
+        
+        foreach (Transform child in buttonD.transform)
+        {
+            child.gameObject.GetComponent<Text>().text = result[0].Properties.OptionFour;
+        }
+
+        if (result[0].Properties.OptionOne.Equals(result[0].Properties.Solution))
+        {
+            sol = 0;
+        }
+        else if (result[0].Properties.OptionTwo.Equals(result[0].Properties.Solution))
+        {
+            sol = 1;
+        }
+        else if (result[0].Properties.OptionThree.Equals(result[0].Properties.Solution))
+        {
+            sol = 2;
+        }
+        else
+        {
+            sol = 3;
+        }
+        //Debug.Log();
+        //StartCoroutine(DownloadImage(result[0].Properties.HelperOne.Images[0].Url));
     }
     
     IEnumerator GetAudioClip(string link)
